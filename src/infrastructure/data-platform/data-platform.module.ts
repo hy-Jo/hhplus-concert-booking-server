4a37d778a57c5b589e1f98237a2a7f1ebc5561f2
HY Jo <jo.hy2503@gmail.com>
2026-02-28 20:25:32 +0900

    feat: Create DataPlatform Kafka Consumer

--- src/infrastructure/data-platform/data-platform.module.ts
+++ src/infrastructure/data-platform/data-platform.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DataPlatformService } from './data-platform.service';
+import { DataPlatformConsumer } from './data-platform.consumer';
 
 @Module({
-  providers: [DataPlatformService],
+  providers: [DataPlatformService, DataPlatformConsumer],
   exports: [DataPlatformService],
 })
 export class DataPlatformModule {}
